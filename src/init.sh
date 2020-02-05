e957e1f9e35bb090e97107a287ebbde2836139fa
Ting <qting0529@hotmail.com>
2020-02-05 17:08:41 +0800

    Fixed wrong package name

--- src/init.sh
+++ src/init.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 sudo apt-get install ros-$ROS_DISTRO-slam-gmapping -y
 sudo apt-get install ros-$ROS_DISTRO-openslam-gmapping -y
-sudo apt-get install ros-$ROS_DISTRO-map_server -y
+sudo apt-get install ros-$ROS_DISTRO-map-server -y
 sudo apt-get install ros-$ROS_DISTRO-amcl -y
 sudo apt-get install ros-$ROS_DISTRO-navigation -y
 cd ~/catkin_ws
